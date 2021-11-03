fc2c0cefe2cc80d16082bc33e82a6bc26bc2c0a4
Diego Ferreira <ferreirabdiego@gmail.com>
2021-11-03 14:31:57 -0300

    Alteração da estrutura de cultos

--- src/components/organisms/meetings/index.jsx
+++ src/components/organisms/meetings/index.jsx
@@ -25,31 +25,39 @@ const style = css`
       }
       padding: 15px;
       text-align: center;
+
+      &::before {
+        margin: 0 auto;
+        margin-bottom: 35px;
+        content: "";
+        display: block;
+        width: 120px;
+        background: var(--black);
+        height: 1px;
+        opacity: .2;
+      }
+      &:first-child {
+        &::before {
+          background: transparent;
+        }
+      }
       @media(min-width: 560px) {
         &::before {
           margin-top: 30px;
           margin-left: -15px;
           margin-bottom: 20px;
-          content: "";
-          display: block;
           position: absolute;
           width: 1px;
-          background: var(--black);
-          height: 60px;
-          opacity: .2;
-        }
-        &:first-child {
-          &::before {
-            background: transparent;
-          }
+          height: 120px;
         }
       }
       p {
         font-style: italic;
+        font-size: 0.8rem;
       }
     }
     h4 {
-      max-width: 70%;
+      max-width: 90%;
       margin: 0 auto;
       margin-bottom: 1rem;
       text-transform: uppercase;
@@ -68,24 +76,40 @@ const Meetings = () => (
           <p>
             Toda terça-feira às<br/>20:00
           </p>
+          <p><strong>Públic Alvo:</strong> <br/>
+          Adolescentes.</p>
         </div>
         <div>
           <h4>Escola Bíblica</h4>
           <p>
             Toda segunda-feira às<br/>20:00
           </p>
+          <p><strong>Públic Alvo:</strong> <br/>
+          Todos os membros, familiares e visitantes.</p>
         </div>
         <div>
           <h4>Culto de Celebração</h4>
           <p>
             Todo domingo às<br/>09:30 | 17:00 | 19:30
           </p>
+          <p><strong>Públic Alvo:</strong> <br/>
+          Todos os membros, familiares e visitantes.</p>
+        </div>
+        <div>
+          <h4>Culto Diamante</h4>
+          <p>
+            2º sábado do mês às<br/>16:30
+          </p>
+          <p><strong>Públic Alvo:</strong> <br/>
+          Pessoas idosas.</p>
         </div>
         <div>
           <h4>Juventude Anunciar</h4>
           <p>
             Todo sábado às<br/>20:00
           </p>
+          <p><strong>Públic Alvo:</strong> <br/>
+          Jovens.</p>
         </div>
       </div>
     </Content>
